2c74eb25ff4d8529601a51442bc16b134a6c9f0a
ShohamD1121 <96658699+ShohamD1121@users.noreply.github.com>
2022-02-01 22:48:29 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.querySelector(".round").addEventListener("click", function () {
         document.querySelector(".left h1").innerHTML = "&dollar;199.99";
         document.querySelector(".mid h1").innerHTML = "&dollar;249.99";
         document.querySelector(".right h1").innerHTML = "&dollar;399.99";
-    } else {
+    } else if (document.querySelector(".left h1").innerHTML === "$199.99") {
         document.querySelector(".left h1").innerHTML = "&dollar;19.99";
         document.querySelector(".mid h1").innerHTML = "&dollar;24.99";
         document.querySelector(".right h1").innerHTML = "&dollar;39.99";
